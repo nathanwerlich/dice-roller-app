e85a2ff75ce2aca9b7558325d6e0182e1bf2fe5e
Nathan Werlich <nathanwerlich@bridge.ufsc.br>
2025-09-01 00:26:10 -0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://dice-roller-app-three.vercel.app/",
+    ],
     methods: ["GET", "POST"],
   },
 });
